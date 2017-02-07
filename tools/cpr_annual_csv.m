fe6c2d83b5fc9b00cafd5176ff77319f5355d626
Rich Inman <rinman24@gmail.com>
2017-02-06 21:51:45 -0800

    Added comments.
    In an attempt to make the code more readable.

--- tools/cpr_annual_csv.m
+++ tools/cpr_annual_csv.m
@@ -1,14 +1,24 @@
 function [] = cpr_annual_csv(year, daysInYear, irrType, ulocs)
 
+% These are the list of locations where data exists
 lat_lon_pairs = csvread('SolarAnywhere_Lat_Lon.csv');
+% String used to programatically access the day of year from .mat files
 s1 = 'DOY';
+% Number of unique locations in the dataset
 for coord = 1:ulocs
+    % Placeholder for the row to be added
     row = [];
+    % Placeholder the the result that will be returned
     res = [];
+    % Grab the location from the from the strings in
+    % 'SolarAnywhere_Lat_Lon.csv'
     lat = lat_lon_pairs(coord, 1);
     lon = lat_lon_pairs(coord, 2);
+    % Build the location string
     loc_str = strcat('N', num2str(lat), 'W', num2str(abs(lon)));
+    % Load the .mat file into variable 
     variable = load(strcat(loc_str, '.mat'));
+    % Build a year string to access tables
     yr_str = strcat('Y', num2str(year));
     for hour = 1:48
         for day = 1:daysInYear
